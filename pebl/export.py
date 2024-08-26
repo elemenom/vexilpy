edaf22f53fe66d79fc4236f650948de378033eb1
elekk <146763980+elemenom@users.noreply.github.com>
2024-08-26 22:39:38 +0200

    Revert "Pull BetterPebl features to Lynq v5"

--- pebl/export.py
+++
@@ -1,8 +0,0 @@
-from lynq.exportablefunction import ExportableFunction
-
-class export:
-  def __init__(exp: ExportableFunction, returner: bool = False) -> None:
-    self.__r: Any = (exp.export_with_default_args_and_return if returner else exp.export_with_default_args)()
-
-  def get(self) -> Any:
-    return self.__r
